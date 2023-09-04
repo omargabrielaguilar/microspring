d636ec76a4ab1ca63c5c88bfcb4e0cc8fff0421b
Omar <saucyfunnel@gmail.com>
2023-09-04 16:37:03 -0500

    microservico en base a mongodb y testcontainer

--- microspring/src/test/java/com/xxx/microspring/MicrospringApplicationTests.java
+++ microspring/src/test/java/com/xxx/microspring/MicrospringApplicationTests.java
@@ -2,12 +2,26 @@ package com.xxx.microspring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class MicrospringApplicationTests {
+    //usar testContainers librey
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0.9");
+
+    static void setProperties(DynamicPropertyRegistry dymDynamicPropertyRegistry){
+        dymDynamicPropertyRegistry.add("spring.data.mongo.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 
     @Test
     void contextLoads() {
     }
+
 
 }
