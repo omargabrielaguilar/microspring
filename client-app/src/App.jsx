9fbe13822f627b46fa9871574ddd3d05db0c7fdb
Omar <saucyfunnel@gmail.com>
2023-12-18 08:56:52 -0500

    env

--- client-app/src/App.jsx
+++ client-app/src/App.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='app'>
-      app
+      <BrowserRouter>
+        <Routes>
+          <Route path="/chat" element={<Chat />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
