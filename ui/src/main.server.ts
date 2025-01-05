71fac2a5b63cb72503bf965d6c14ba4472b6de43
Omar <saucyfunnel@gmail.com>
2025-01-05 18:36:42 -0500

    avance de cada uno de la ui

--- ui/src/main.server.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-
-// Bootstrap para el servidor
-const bootstrap = () => bootstrapApplication(AppComponent);
-
-export default bootstrap;
--- ui/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-
-// Bootstrap para el navegador
-bootstrapApplication(AppComponent)
-  .catch((err) => console.error(err));
